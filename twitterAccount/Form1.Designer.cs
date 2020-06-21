844f4050c431704d6bb41e6d9abfd14c9e95970f
aliozgunay <65774402+aliozgunay@users.noreply.github.com>
2020-06-21 21:53:22 +0300

    Twitter Side
    Twitter kısmı ali özgünay tarafından yapılmıştır.

---
+++ twitterAccount/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace twitterAccount
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.login = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 576);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tweet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(221, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(931, 121);
+            this.listBox1.TabIndex = 3;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(221, 500);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(156, 29);
+            this.login.TabIndex = 4;
+            this.login.Text = "Timeline";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(221, 173);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(931, 121);
+            this.listBox2.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(192, 108);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(221, 334);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(931, 121);
+            this.listBox3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 500);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 27);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(221, 576);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 29);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "My Tweets";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(445, 503);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(167, 26);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Favorite";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(13, 148);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(191, 95);
+            this.listBox5.TabIndex = 12;
+            this.listBox5.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(445, 576);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(167, 29);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Retweet";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(651, 500);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(166, 29);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Search";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(651, 576);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(166, 27);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "My Favorites";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(854, 500);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(172, 31);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "My Retweets";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 653);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ twitterAccount/Form1.cs
@@ -0,0 +1,135 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TweetSharp;
+using TweetSharp.Model;
+
+
+
+
+namespace twitterAccount
+{
+    public partial class Form1 : Form
+    {
+        TwitterService servis;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+            servis = new TwitterService("dTMK6m1S5ESaAYOkD7uO01Way", "DSRXujpG26GIbttWc0Zxc4zUA6hyWeo04Ik3P3dMabHN9GDQXp", "609285151-2YteSBo7xkv1PoGGQlukfMH1yi9J3QL3adYKJbbV", "mv1gUKpT66dCQBcWAI0itFKYYdjNU1B2kRubqx4psWoGE");
+            //var requesttoken = servis.GetRequestToken();
+            //var uri = servis.GetAuthorizationUri(requesttoken);
+            //Uri uri = servis.GetAuthenticationUrl(requesttoken);
+            //Process.Start(uri.ToString());
+            //VerifyCredentialsOptions option = new VerifyCredentialsOptions();
+            //TwitterUser user = servis.VerifyCredentials(option);
+            //var access = servis.GetAccessToken(requesttoken);
+            //OAuthAccessToken accessToken = servis.GetAccessToken(requesttoken, "aEFizII0hIvnqJd1P99WjjBqoab1Mf5y");
+
+
+        }
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+       
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var tweetat = servis.SendTweet(new SendTweetOptions() { Status = richTextBox1.Text });
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var tweetal = servis.ListTweetsOnHomeTimeline(new ListTweetsOnHomeTimelineOptions { Count = 5 });
+            foreach (var tweet in tweetal)
+            {
+                listBox1.Items.Add(tweet.User.Name + " : " + tweet.Text);
+                listBox5.Items.Add(tweet.Id);
+            }
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox5.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var tweetal = servis.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions { Count = 5 });
+            foreach (var tweet in tweetal)
+            {
+                listBox1.Items.Add(tweet.User.Name + " : " + tweet.Text);
+                listBox5.Items.Add(tweet.Id);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            listBox5.SelectedIndex = listBox1.SelectedIndex;
+  
+
+            var fav = servis.FavoriteTweet(new FavoriteTweetOptions() { Id = long.Parse(listBox5.SelectedItem.ToString()) });
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox5.SelectedIndex = listBox1.SelectedIndex;
+
+
+            var retweet = servis.Retweet(new RetweetOptions() { Id = long.Parse(listBox5.SelectedItem.ToString()) });
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var search = servis.Search(new SearchOptions() { Q = richTextBox1.Text });
+            foreach (var tweet in search.Statuses)
+            {
+                listBox1.Items.Add(tweet.User.Name + " : " + tweet.Text);
+                listBox5.Items.Add(tweet.Id);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var getFav = servis.ListFavoriteTweets(new ListFavoriteTweetsOptions { Count = 5 });
+            foreach (var tweet in getFav)
+            {
+                listBox2.Items.Add(tweet.User.Name + " : " + tweet.Text);
+                
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var getRet = servis.ListRetweetsOfMyTweets(new ListRetweetsOfMyTweetsOptions { Count = 5 });
+            foreach (var tweet in getRet)
+            {
+                listBox3.Items.Add(tweet.User.Name + " : " + tweet.Text);
+
+            }
+        }
+    }
+}
